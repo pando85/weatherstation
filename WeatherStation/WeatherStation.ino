e12c1bcdb246f69a8002a7f69ef3c8e01a39092c
Pando85 <pando855@gmail.com>
2018-08-05 22:03:13 +0200

    Trying new travis.yml

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -33,30 +33,7 @@ void typeof(unsigned long a)   { Serial.print("\tit is a unsigned long");}
 void typeof(float a)   { Serial.print("\tit is a float");}
 void typeof(char* a) { Serial.print("\tit is a char*");}
 
-void MQTT_connect() {
-  int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    return;
-  }
-
-  Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
-  }
-  Serial.println("MQTT Connected!");
-}
+void MQTT_connect(void);
 
 void setup() {
   Serial.begin(9600);
@@ -161,3 +138,30 @@ void loop() {
 
   delay(2000);
 }
+
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
+
